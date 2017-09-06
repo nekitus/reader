1a98c2fe6a8df43c5ad7f9904c84f343a83beb98
nikita <nikita@MacBook-Pro-1.local>
2017-09-07 01:26:20 +0300

    small fix

--- server/app.js
+++ server/app.js
@@ -32,7 +32,6 @@ if(isDevelopment) {
     app.use(express.static(path.join(__dirname, '../dist')));
 }
 
-
 // view engine setup
 //app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'jade');
