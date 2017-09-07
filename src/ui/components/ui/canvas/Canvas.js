aff4e82406a63ae867a5bd237a986436aea3fb15
nikita <nikita@MacBook-Pro-1.local>
2017-09-07 22:09:59 +0300

    add PIXI components to learning

--- src/ui/components/ui/canvas/Canvas.js
+++ src/ui/components/ui/canvas/Canvas.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import "pixi.js"
 
+const COLOR_MAX = 255;
+
 export default class Canvas extends Component {
 
     static propTypes = {
@@ -8,69 +10,56 @@ export default class Canvas extends Component {
 
     constructor( props ) {
         super(props);
-
-        //bind our animate function
-        this.animate = this.animate.bind(this);
-        //bind our zoom function
-        this.updateZoomLevel = this.updateZoomLevel.bind(this);
     }
 
-    weightToImageData(weights) {
+    weightToSprite(weights) {
         const data = weights.data;
-
-        var c = document.createElement("canvas");
-        var ctx = c.getContext("2d");
-        var imgData = ctx.createImageData(50,50);
-        for (let i = 0, j = 0;i<imgData.data.length; i += 4, j++) {
-            imgData.data[i+0]= (data[j] * -255 / 4) + 255;
-            imgData.data[i+1]= (data[j] * -255 / 4) + 255;
-            imgData.data[i+2]= (data[j] * -255 / 4) + 255;
-            imgData.data[i+3]= 160;
+        let c = document.createElement("canvas");
+        let ctx = c.getContext("2d");
+        let imgData = ctx.createImageData(50, 50);
+        for (let i = 0, j = 0; i < imgData.data.length; i += 4, j++) {
+            let color = weightToRGBColor(data[j]);
+            imgData.data[i]     = color;
+            imgData.data[i + 1] = color;
+            imgData.data[i + 2] = color;
+            imgData.data[i + 3] = 160;
         }
-        return imgData
+        ctx.putImageData(imgData, 1, 1);
+        return PIXI.Sprite.from(c.toDataURL());
     }
 
     componentDidMount() {
-        this.canvas = document.createElement('canvas');
-        this.canvas.width = 100;
-        this.canvas.height = 1000;
-        this.refs.gameCanvas.appendChild(this.canvas);
-
-        this.context = this.canvas.getContext('2d');
+        var app = new PIXI.Application(600, 800, {backgroundColor : 0xFFFFFF});
+        this.refs.learnCanvas.appendChild(app.view);
         const weights = this.props.weights;
         for(let i = 0; i < weights.length; i++) {
-            const imageData = this.weightToImageData(weights[i]);
-            this.context.translate(0, 55 * i);
-            this.context.putImageData(imageData, 0, 55 * i);
+            let container = new PIXI.Container();
+            const char = this.weightToSprite(weights[i]);
+            const title = new PIXI.Text(weights[i].name);
+
+            var graphics = new PIXI.Graphics();
+            graphics.beginFill(0xEEEEEE);
+            graphics.drawRect(0, 0, 200, 60);
+
+
+            title.position.x = 60;
+            title.position.y = 10;
+            container.addChild(graphics);
+            container.addChild(title);
+            container.addChild(char);
+            container.position.x = 30;
+            container.position.y = (60 * i) + 60;
+            app.stage.addChild(container);
         }
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        //this is easy with 1 prop, using Immutable helpers make
-        //this easier to scale
-
-        return nextProps.zoomLevel !== this.props.zoomLevel;
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.updateZoomLevel(nextProps);
-    }
-
-    updateZoomLevel(props) {
-        this.stage.scale.x = props.zoomLevel;
-        this.stage.scale.y = props.zoomLevel;
-    }
-
-    animate() {
-        // render the stage container
-        //this.renderer.render(this.stage);
-        //this.frame = requestAnimationFrame(this.animate);
     }
 
     render() {
         return (
-            <div className="game-canvas-container" ref="gameCanvas">
-            </div>
+            <div ref="learnCanvas"></div>
         );
     }
+}
+
+function weightToRGBColor(weight) {
+    return (weight * -COLOR_MAX / 4) + COLOR_MAX
 }
